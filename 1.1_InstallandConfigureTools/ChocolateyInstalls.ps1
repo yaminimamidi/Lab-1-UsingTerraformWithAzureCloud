e39af7bb5d6313e86939cea07d2b62663e2e8971
Matt McElreath <matt.mcelreath@gmail.com>
2022-05-20 16:35:55 -0400

    Updated Choc Installs Comment

--- 1.1_InstallandConfigureTools/ChocolateyInstalls.ps1
+++ 1.1_InstallandConfigureTools/ChocolateyInstalls.ps1
@@ -1,3 +1,9 @@
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#
+# All Commands must be run from a terminal using 'Run As Administrator' 
+#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 # Install Choclatey
 # https://chocolatey.org/install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
