d39d9c2c907674367a944c3c93bdbb99f971ce90
Matt McElreath <matt.mcelreath@gmail.com>
2022-07-07 15:24:27 -0400

    Removed Examples

--- 1.9_PublishingModules/examples/example1/main.tf
+++
@@ -1,7 +0,0 @@
-module "acstoragetest" {
-  source               = "mmcelreath/acstoragetest/azurerm"
-  version              = "0.0.1"
-  resource_group_name  = "RG-STORAGE-01"
-  storage_account_name = "acstoragetest01"
-  location             = "eastus2"
-}
