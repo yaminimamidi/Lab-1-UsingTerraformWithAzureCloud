2f4b45feaee2746a6b885842beee546b5c9c3b97
Matt McElreath <matt.mcelreath@gmail.com>
2022-07-17 17:26:51 -0400

    Updated 1.8

--- 1.8_CreatingLocalModules/main.tf
+++ 1.8_CreatingLocalModules/main.tf
@@ -1,14 +1,14 @@
 module "Storage1" {
   source               = "./modules/StorageAccount"
   resource_group_name  = "RG-STORAGE-01"
-  storage_account_name = "acteststorage01"
+  storage_account_name = "acteststorage001"
   location             = "EastUs2"
 }
 
 module "Storage2" {
   source               = "./modules/StorageAccount"
   resource_group_name  = "RG-STORAGE-02"
-  storage_account_name = "acteststorage02"
+  storage_account_name = "acteststorage002"
   location             = "EastUs2"
 }
 
@@ -25,36 +25,3 @@ resource "azurerm_storage_container" "content" {
 output "Storage2" {
   value = module.Storage2
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# resource "azurerm_resource_group" "rg" {
-#   name     = "RG-01"
-#   location = "eastus2"
-
-#   tags = {
-#     Demo = "true"
-#   }
-# }
-
-# resource "azurerm_storage_account" "sa" {
-#   name                     = "acteststorage01"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-
-#   tags = azurerm_resource_group.rg.tags
-# }
