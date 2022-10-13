d3b8dae2389dc460fcf30e4bb0f5f6880930dfda
Matt McElreath <matt.mcelreath@gmail.com>
2022-10-13 17:08:18 -0400

    Updated Choc Installs Comment

--- 1.1_InstallandConfigureTools/ChocolateyInstalls.ps1
+++ 1.1_InstallandConfigureTools/ChocolateyInstalls.ps1
@@ -23,4 +23,3 @@ choco install vscode -y --force
 # Install PoweShell 7 (Core) - OPTIONAL
 # https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows
 choco install powershell-core -y --force
-
